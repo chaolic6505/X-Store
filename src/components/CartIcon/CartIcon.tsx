9a81c48e952878d4b06b5ea7328483935ac89a0c
SC <chaolic6505@gmail.com>
2021-06-05 23:57:18 -0700

    get  cart items count with selectors

--- src/components/CartIcon/CartIcon.tsx
+++ src/components/CartIcon/CartIcon.tsx
@@ -2,21 +2,25 @@ import React from 'react';
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/CartActions';
+import { selectCartItemsCount } from '../../redux/cart/CartSelectors';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './CartIcon.scss';
 
-interface ICartIcon {
-	toggleCartHidden: () => {};
-}
-const CartIcon: React.FC<ICartIcon> = ({ toggleCartHidden }) => (
+const CartIcon: React.FC<{ toggleCartHidden; itemCount }> = ({
+	toggleCartHidden,
+	itemCount,
+}) => (
 	<div className="cart-icon" onClick={toggleCartHidden}>
 		<ShoppingIcon className="shopping-icon" />
-		<span className="item-count">0</span>
+		<span className="item-count">{itemCount}</span>
 	</div>
 );
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
 	toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
+const mapStateToProps = (state) => ({
+	itemCount: selectCartItemsCount(state),
+});
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
